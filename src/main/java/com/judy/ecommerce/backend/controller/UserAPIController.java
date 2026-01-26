34a4e97c251558c21d344c89e813f923acb142bf
Cocojuju13 <julien.mouzet@coda-student.school>
2026-01-26 11:39:05 +0100

    Add the remaining user routes (edit password + delete) + admin routes

--- src/main/java/com/judy/ecommerce/backend/controller/UserAPIController.java
+++ src/main/java/com/judy/ecommerce/backend/controller/UserAPIController.java
@@ -2,8 +2,10 @@ package com.judy.ecommerce.backend.controller;
 
 import com.judy.ecommerce.backend.dto.user.EditUserDTO;
 import com.judy.ecommerce.backend.dto.user.EmailDTO;
+import com.judy.ecommerce.backend.dto.user.PasswordDTO;
 import com.judy.ecommerce.backend.dto.user.UserDTO;
 import com.judy.ecommerce.backend.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -26,14 +28,27 @@ public class UserAPIController {
 
     @PatchMapping()
     public ResponseEntity<UserDTO> updateSelfInfo(@AuthenticationPrincipal UserDetails userDetails,
-                                                  @RequestBody EditUserDTO editUserDTO) {
+                                                  @RequestBody @Valid EditUserDTO editUserDTO) {
         return ResponseEntity.ok(userService.editSelfInfo(userDetails, editUserDTO));
     }
 
     @PatchMapping("/email")
     public ResponseEntity<String> updateSelfEmail(@AuthenticationPrincipal UserDetails userDetails,
-                                                  @RequestBody EmailDTO emailDTO) {
+                                                  @RequestBody @Valid EmailDTO emailDTO) {
         userService.editSelfEmail(userDetails, emailDTO);
         return ResponseEntity.ok("Email changed.");
+    }
+
+    @PatchMapping("/password")
+    public ResponseEntity<String> updateSelfPassword(@AuthenticationPrincipal UserDetails userDetails,
+                                                     @RequestBody @Valid PasswordDTO passwordDTO) {
+        userService.editSelfPassword(userDetails, passwordDTO);
+        return ResponseEntity.ok("Password changed.");
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<String> deleteSelfUser(@AuthenticationPrincipal UserDetails userDetails) {
+        userService.deleteSelfUser(userDetails);
+        return ResponseEntity.ok("Account deleted successfully.");
     }
 }
--- src/main/java/com/judy/ecommerce/backend/service/UserService.java
+++ src/main/java/com/judy/ecommerce/backend/service/UserService.java
@@ -1,22 +1,32 @@
 package com.judy.ecommerce.backend.service;
 
+import com.judy.ecommerce.backend.RoleEnum;
 import com.judy.ecommerce.backend.dto.user.EditUserDTO;
 import com.judy.ecommerce.backend.dto.user.EmailDTO;
+import com.judy.ecommerce.backend.dto.user.PasswordDTO;
 import com.judy.ecommerce.backend.dto.user.UserDTO;
 import com.judy.ecommerce.backend.entity.Users;
-import com.judy.ecommerce.backend.exception.ConflictException;
-import com.judy.ecommerce.backend.exception.InvalidFormatException;
+import com.judy.ecommerce.backend.exception.*;
 import com.judy.ecommerce.backend.repository.UserRepository;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UserDTO getSelfInfo(UserDetails userDetails) {
@@ -59,6 +69,10 @@ public class UserService {
         Users user = userRepository.findByEmailIgnoreCaseAndDisabledFalse(userDetails.getUsername())
                 .orElseThrow();
 
+        if (Objects.equals(user.getEmail(), emailDTO.email())) {
+            throw new NoModificationsException("No modification applied.");
+        }
+
         // Check email format, also check and disallow + aliases, + check uniqueness
         if (!emailDTO.email().matches("^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
             throw new InvalidFormatException("Invalid email format.");
@@ -71,6 +85,88 @@ public class UserService {
         user.setEmail(emailDTO.email());
 
         userRepository.save(user);
+    }
+
+    public void editSelfPassword(UserDetails userDetails, PasswordDTO passwordDTO) {
+        // Shouldn't fail
+        Users user = userRepository.findByEmailIgnoreCaseAndDisabledFalse(userDetails.getUsername())
+                .orElseThrow();
+
+        if (!passwordEncoder.matches(passwordDTO.currentPassword(), user.getPassword())) {
+            throw new BadCredentialsException("Incorrect password.");
+        }
+
+        user.setPassword(passwordEncoder.encode(passwordDTO.newPassword()));
+        userRepository.save(user);
+    }
+
+    public void deleteSelfUser(UserDetails userDetails) {
+        // Shouldn't fail
+        Users user = userRepository.findByEmailIgnoreCaseAndDisabledFalse(userDetails.getUsername())
+                .orElseThrow();
+
+        // Make sure the user table contains at least another admin to avoid being locked out
+        if (userRepository.countUsersByRoleIs(RoleEnum.ADMIN) < 2) {
+            throw new UnauthorizedException("Cannot disable user, there must be at least 1 active admin.");
+        }
+
+        user.setDisabled(true);
+        userRepository.save(user);
+    }
+
+
+    // ADMIN //
+
+    public List<UserDTO> getAllUsersAdmin() {
+        List<Users> users = userRepository.findAll();
+
+        return listToDTO(users);
+    }
+
+    public UserDTO getUserByIdAdmin(long id) {
+        // Ignore disabled check
+        Users user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found."));
+
+        return userToDTO(user);
+    }
+
+    public void deleteUserByIdAdmin(UserDetails userDetails, long id) {
+        // Shouldn't fail
+        Users selfUser = userRepository.findByEmailIgnoreCaseAndDisabledFalse(userDetails.getUsername())
+                .orElseThrow();
+
+        // Do not disable its own account this way
+        if (selfUser.getId() == id) {
+            throw new UnauthorizedException("Cannot disable your account this way.");
+        }
+
+        // Get the account to disable
+        Users user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found."));
+
+        user.setDisabled(true);
+        userRepository.save(user);
+    }
+
+    public UserDTO editRoleAdmin(UserDetails userDetails, long id, RoleEnum role) {
+        // Shouldn't fail
+        Users selfUser = userRepository.findByEmailIgnoreCaseAndDisabledFalse(userDetails.getUsername())
+                .orElseThrow();
+
+        // Do not change your own role
+        if (selfUser.getId() == id) {
+            throw new UnauthorizedException("Cannot change your own role.");
+        }
+
+        // Get the account to change
+        Users user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found."));
+
+        user.setRole(role);
+        userRepository.save(user);
+
+        return userToDTO(user);
     }
 
 
@@ -87,5 +183,15 @@ public class UserService {
                 user.getUpdatedAt(),
                 user.isDisabled()
         );
+    }
+
+    private List<UserDTO> listToDTO(List<Users> listUsers) {
+        List<UserDTO> listDTO = new ArrayList<>();
+
+        for (Users user : listUsers) {
+            listDTO.add(userToDTO(user));
+        }
+
+        return listDTO;
     }
 }
