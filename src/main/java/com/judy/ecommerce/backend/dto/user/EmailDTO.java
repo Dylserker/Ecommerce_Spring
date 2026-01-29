aa5f9e2900d4023009114d8bb78d173fb77bc7c5
Cocojuju13 <julien.mouzet@coda-student.school>
2026-01-29 11:41:43 +0100

    Fused route to change email with route to change user infos

--- src/main/java/com/judy/ecommerce/backend/dto/user/EmailDTO.java
+++
@@ -1,8 +0,0 @@
-package com.judy.ecommerce.backend.dto.user;
-
-import jakarta.validation.constraints.Size;
-import jakarta.validation.constraints.NotBlank;
-
-public record EmailDTO(
-        @NotBlank @Size(max=255) String email
-) {}
